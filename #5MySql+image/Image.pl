dceac82dec18044379a0dc156460049ae7c06da6
Kalifornia2019 <kachan.84123456789@ukr.net>
2019-11-04 10:36:38 +0200

    Change

--- #5MySql+image/Image.pl
+++ #5MySql+image/Image.pl
@@ -1,30 +1,30 @@
-#!/usr/bin/perl
+  #!/usr/bin/perl
 
-use strict;
-use DBI;
+  use strict;
+  use DBI;
 
-#Встанолюємо зєднання з БД
-my $dbh = DBI->connect(          
+  #Встанолюємо зєднання з БД
+  my $dbh = DBI->connect(          
     "dbi:mysql:dbname=perl", 
     "root",                          
     "9101984dvir",                          
     { RaiseError => 1 },         
-) or die $DBI::errstr;
+  ) or die $DBI::errstr;
 
-#Відкриваємо зображення
-open IMAGE, "image.png" or die $!;
+  #Відкриваємо зображення
+  open IMAGE, "image.png" or die $!;
 
-#Зчитуємо двійкові дані з файлу зображення
-my ($image, $buff);
-while(read IMAGE, $buff, 1024) {
+  #Зчитуємо двійкові дані з файлу зображення
+  my ($image, $buff);
+  while(read IMAGE, $buff, 1024) {
     $image .= $buff;
-}
+  }
 
-#Вставляємо зображення в табл. Images
-my $stm = $dbh->prepare("INSERT INTO Images(Id, Data) VALUES (1, ?)");
-$stm->bind_param(1, $image, DBI::SQL_BLOB);
-$stm->execute();
+  #Вставляємо зображення в табл. Images
+  my $stm = $dbh->prepare("INSERT INTO Images(Id, Data) VALUES (1, ?)");
+  $stm->bind_param(1, $image, DBI::SQL_BLOB);
+  $stm->execute();
 
-close(IMAGE);
-$stm->finish();
-$dbh->disconnect();
+  close(IMAGE);
+  $stm->finish();
+  $dbh->disconnect();
